9cf44bbfa55112aefe3ff2911c13d5c8750a2673
Aly M. Ashraf <captainslowg@gmail.com>
2022-10-17 02:17:57 +0200

    some methods added in ChessGame class

---
+++ AsymmetricEncryption.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Encryptor
+{
+    internal class AsymmetricEncryption
+    {
+        public RSA rsaBlob;
+        public Aes aesBlob;
+
+        public string publicKey;
+        public byte[] publicKeyByte;
+
+        public string privateKey;
+        public byte[] privateKeyByte;
+
+        public string clearText;
+        public byte[] clearTextByte;
+
+        public string cipherText;
+        public byte[] cipherTextByte;
+
+        public string initVec;
+        public byte[] initVecByte;
+
+
+
+        public AsymmetricEncryption()
+        {
+            RSA placeholderRsa = RSA.Create();
+            rsaBlob = placeholderRsa;
+
+            Aes placeholderAes = Aes.Create();
+            aesBlob = placeholderAes;
+
+        }
+    }
+}
